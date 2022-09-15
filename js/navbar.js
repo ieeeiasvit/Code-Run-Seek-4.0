7e9f03650a2420dfae87b8fc6f62e14318045da0
saiharshavarma <saiharshavarma@outlook.com>
2022-09-15 21:05:31 +0530

    IAS logo movement

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,3 @@
 document.querySelector(".icon").addEventListener("click", function () {
-  document.querySelector(".sidebar").classList.toggle("toggle2");
-  document.querySelector(".ias").classList.toggle("toggle1");
+    document.querySelector(".sidebar").classList.toggle("toggle2");
 });
-
